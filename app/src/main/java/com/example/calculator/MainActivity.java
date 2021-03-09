591ac5068a8d557cc698870ce2ebe9e7b9433375
Alex Radionov <RadionovAlex1@gmail.com>
2021-03-10 02:45:51 +0300

    3 commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static com.example.calculator.R.*;
 
 public class MainActivity extends AppCompatActivity {
     private String stringInput = ""; // строка вводимая пользователем
@@ -16,278 +22,227 @@ public class MainActivity extends AppCompatActivity {
     private final String sqr = "√";
     private final String multiply = "×";
     private final String div = "÷";
+    private final String colorOperator = "#8D54F1";
+
+    private long backPrassedTime;
 
     private TextView textView_line_input; // для вывода и отображения вводимой пользователем информации
+    private ScrollView scrollView;
 
     private Toast toastLimit;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
 
-        textView_line_input = (TextView) findViewById(R.id.textView_line_input);
+        textView_line_input = (TextView) findViewById(id.textView_line_input);
+        scrollView = (ScrollView) findViewById(id.scrollView1);
 
-        ImageView img_one = (ImageView) findViewById(R.id.number_one);
+
+        ImageView img_one = (ImageView) findViewById(id.number_one);
         img_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("1");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_two = (ImageView) findViewById(R.id.number_two);
+        ImageView img_two = (ImageView) findViewById(id.number_two);
         img_two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("2");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_three = (ImageView) findViewById(R.id.number_three);
+        ImageView img_three = (ImageView) findViewById(id.number_three);
         img_three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("3");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_four = (ImageView) findViewById(R.id.number_four);
+        ImageView img_four = (ImageView) findViewById(id.number_four);
         img_four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("4");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_five = (ImageView) findViewById(R.id.number_five);
+        ImageView img_five = (ImageView) findViewById(id.number_five);
         img_five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("5");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){}
             }
         });
 
-        ImageView img_six = (ImageView) findViewById(R.id.number_six);
+        ImageView img_six = (ImageView) findViewById(id.number_six);
         img_six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("6");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){}
             }
         });
 
-        ImageView img_seven = (ImageView) findViewById(R.id.number_seven);
+        ImageView img_seven = (ImageView) findViewById(id.number_seven);
         img_seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("7");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_eight = (ImageView) findViewById(R.id.number_eight);
+        ImageView img_eight = (ImageView) findViewById(id.number_eight);
         img_eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("8");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_zero = (ImageView) findViewById(R.id.number_zero);
+        ImageView img_zero = (ImageView) findViewById(id.number_zero);
         img_zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("0");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){  }
             }
         });
 
-        ImageView img_nine = (ImageView) findViewById(R.id.number_nine);
+        ImageView img_nine = (ImageView) findViewById(id.number_nine);
         img_nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addNumber("9");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_point = (ImageView) findViewById(R.id.point);
+        ImageView img_point = (ImageView) findViewById(id.point);
         img_point.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addPoint();
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_plus = (ImageView) findViewById(R.id.plus);
+        ImageView img_plus = (ImageView) findViewById(id.plus);
         img_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addOperation("+");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_minus = (ImageView) findViewById(R.id.minus);
+        ImageView img_minus = (ImageView) findViewById(id.minus);
         img_minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addOperation("-");
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView img_multiply = (ImageView) findViewById(R.id.multiply);
+        ImageView img_multiply = (ImageView) findViewById(id.multiply);
         img_multiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addOperation(multiply);
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){}
             }
         });
 
-        ImageView img_divide = (ImageView) findViewById(R.id.divide);
+        ImageView img_divide = (ImageView) findViewById(id.divide);
         img_divide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addOperation(div);
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
-        ImageView erase = (ImageView) findViewById(R.id.erase);
+        ImageView erase = (ImageView) findViewById(id.erase);
         erase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     stringDelNum();
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                    printLineOnDisplay();
+                }catch (Exception e){ }
             }
         });
 
-        ImageView clear = (ImageView) findViewById(R.id.clear);
+        ImageView clear = (ImageView) findViewById(id.clear);
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     stringInput = "";
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                    printLineOnDisplay();
+                }catch (Exception e){ }
             }
         });
 
         // добавляем скобки "(" или ")"
-        ImageView parentheses = (ImageView) findViewById(R.id.parentheses);
+        ImageView parentheses = (ImageView) findViewById(id.parentheses);
         parentheses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addBracket();
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
         // добавляем скобки корень
-        ImageView square_root = (ImageView) findViewById(R.id.square_root);
+        ImageView square_root = (ImageView) findViewById(id.square_root);
         square_root.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addSqrt();
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
 
         // добавляем скобки корень
-        ImageView sign = (ImageView) findViewById(R.id.sign);
+        ImageView sign = (ImageView) findViewById(id.sign);
         sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     addSignBeforeNumber();
-                    textView_line_input.setText(stringInput);
-                }catch (Exception e){
-
-                }
+                }catch (Exception e){ }
             }
         });
     }
@@ -322,17 +277,21 @@ public class MainActivity extends AppCompatActivity {
             String lastNumber = lastNumber();
             if(lastNumber.length() < 15){
                 newResultString(st);
+
             }else{
                 // вывести сообщение что превышена максимальная длинна для числа
-                toastLimit = Toast.makeText(getBaseContext(), R.string.number_limit,
+                toastLimit = Toast.makeText(getBaseContext(), string.number_limit,
                         Toast.LENGTH_SHORT);
                 toastLimit.show();
             }
         }else if(isLastCharacterBracket2()){
             newResultString(multiply + st);  // если последний символ закрывающая скобка, то добавляем с умножением
+
         }else{
             newResultString(st);
+
         }
+       printLineOnDisplay();
     }
 
     // метод добавляет знак для числа
@@ -381,7 +340,7 @@ public class MainActivity extends AppCompatActivity {
         }else if(isLastCharacterBracket2()){
             newResultString(multiply + "(-");
         }
-
+        printLineOnDisplay();
     }
 
     // метод для добавления в строку операций +, -, *, ÷
@@ -399,6 +358,7 @@ public class MainActivity extends AppCompatActivity {
                 newResultString(st);                             // заменяем на новый символ
             }
         }
+        printLineOnDisplay();
     }
 
     // метод добавляет конень в строку. если строка пустая добавляем корень.
@@ -411,6 +371,7 @@ public class MainActivity extends AppCompatActivity {
         }else if(isLastCharacterNumber() || isLastCharacterBracket2()){
             newResultString(multiply + sqr + "(");
         }
+        printLineOnDisplay();
     }
 
     // метод для добавления скобок в строку
@@ -435,6 +396,7 @@ public class MainActivity extends AppCompatActivity {
                 newResultString(")");
             }
         }
+        printLineOnDisplay();
     }
 
     // метод для добавления точки в строку, для ввода дробного числа
@@ -455,6 +417,7 @@ public class MainActivity extends AppCompatActivity {
                 newResultString(".");
             }
         }
+        printLineOnDisplay();
     }
 
     // метод проверяет является ли последний символ операцией "+", "-", "÷", "*" у строки stringInput
@@ -601,5 +564,65 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return res;
+    }
+
+
+    // системная кнопка назад
+    @Override
+    public void onBackPressed() {
+        if(backPrassedTime + 2000 > System.currentTimeMillis()){
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        }else {
+            backToast = Toast.makeText(getBaseContext(), string.exit,
+                    Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPrassedTime = System.currentTimeMillis();
+    }
+
+    // выводим добавленные символы на экран
+    private void printLineOnDisplay(){
+        if(stringInput.length() > 15){   // если строка большая, то уменьшить размер выводимого шрифта
+            textView_line_input.setTextSize(30);
+        }else {
+            textView_line_input.setTextSize(40);
+        }
+
+        ArrayList<Integer> list = arrayIndexAllOperator(); // получаем список всех порядковых индексов всех не чисел
+
+        SpannableString text = new SpannableString(stringInput);  // расскрасим все элементы не являющиеся числами в фиолетовый
+        if(list.size() > 0){
+            int a;
+            for(int i = 0; i < list.size(); i++) {
+                a = list.get(i);
+                text.setSpan(new ForegroundColorSpan(Color.parseColor(colorOperator)), a, a + 1, 0);
+            }
+        }
+        textView_line_input.setText(text, TextView.BufferType.SPANNABLE);
+//        scrollView.fullScroll(View.FOCUS_DOWN);
+        scrollView.pageScroll(View.FOCUS_DOWN);   // для автоматической прокрутке во время ввода строки
+    }
+
+    // метод возвращает список состоящий из чисел, которые являются порядковыми индексами элементов в строке stringInput
+    // собирает все кроме чисел (точка тоже часть числа и не входит)
+    // "(" == 40 ; ")" == 41;
+    private ArrayList<Integer> arrayIndexAllOperator(){
+        ArrayList<Integer> list = new ArrayList<>();
+        int l = stringInput.length();
+        char [] chars = stringInput.toCharArray();
+        char ch;
+        char multCr = multiply.charAt(0);
+        char divCh = div.charAt(0);
+        char sqrCh = sqr.charAt(0);
+            for(int i = 0; i < l; i++){
+                ch = chars[i];
+                if(ch == 40 || ch == 41 || ch == 43 || ch == 45 || ch == divCh || ch == multCr || ch == sqrCh){
+                    list.add(i);
+                }
+            }
+
+        return list;
     }
 }
